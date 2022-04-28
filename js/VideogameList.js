2cfaae567ca96960e66285b6fb424a4ef9f1ccbc
Arizxd09 <ar.araya@duocuc.cl>
2022-04-28 17:17:24 -0400

    Se agrega utilización de API de juegos
    Se agrega api de nvidia para listar todos los juegos publicos

---
+++ js/VideogameList.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $.get("https://static.nvidiagrid.net/supported-public-game-list/gfnpc.json?JSON", function (data){
+        $.each(data,function(i,item){
+            $("#Juegos").append(
+                "<div class='col-12 textAlignCenter backgroundColorListaGame'>" +
+                    "<h3 class='titleListaGames h3'>Titulo De Juego: </h1><p class='listaGames'>" + item.title +
+                "</p><h3 class='titleListaGames h3'>URL de Steam: </h1><p class='listaGames'><a href='" + item.steamUrl + "'>" + item.steamUrl + "</a>" +
+                "</p><h3 class='titleListaGames h3'>Editor: </h1><p class='listaGames'>" + item.publisher +
+                "</p><h3 class='titleListaGames h3'>Género(s): </h1><p class='listaGames'>" + item.genres +
+                "</p></div><div class='padding10'></div>"
+            );
+        });
+    });
+});
